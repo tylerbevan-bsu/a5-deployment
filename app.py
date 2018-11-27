6a2f88ecdeff0bf877001688aa06ed69d004e1ed
Tyler Bevan <tylerbevan@boisestate.edu>
2018-11-26 18:27:26 -0700

    Sessions added

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, make_response, Response
+from flask_session import Session
 from model import Model
 
 app = Flask(__name__)
 
-
-model = Model()
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
 
 @app.route('/')
 def homepage():
-    global model
+    session['model'] = Model()
 
     return """
     <h1>Sentiment Decider</h1>
@@ -19,58 +21,60 @@ def homepage():
     <form method="get" action="/trainonehot">
         <button type="submit">Train One-Hot</button>
     </form>
-    <form method="get" action="/test">
-        <button type="submit">Test Current Model</button>
-    </form>
     """.format(trained=model.trained)
 
 @app.route('/trainw2v')
 def train_w2v():
-    global model
-    model.train_w2v()
+    session['model'].train_w2v()
     return """
         <h1>Trained Word 2 Vec</h1>
         <form method="get" action="/test">
             <button type="submit">Test Model</button>
         </form>
         <form method="get" action="/">
-            <button type="submit">Go Home</button>
+            <button type="submit">Restart</button>
         </form>
         """ 
 
 @app.route('/trainonehot')
 def train_onehot():
-    global model
-    model.train_onehot()
+    session['model'].train_onehot()
     return """
         <h1>Trained One-Hot</h1>
         <form method="get" action="/test">
             <button type="submit">Test Model</button>
         </form>
         <form method="get" action="/">
-            <button type="submit">Go Home</button>
+            <button type="submit">Restart</button>
         </form>
         """ 
 
 @app.route('/test')
 def test():
-    global model
-    if model.model is None:
+    if not 'model' in session:
         return make_response("""
             <h1>Model Not Trained</h1>
             <p>Please Train a Model</p>
             <form method="get" action="/">
-                <button type="submit">Go Home</button>
+                <button type="submit">Restart</button>
             </form>
             """, 400)
-    score = model.test_model()
+    if session['model'] is None:
+        return make_response("""
+            <h1>Model Not Trained</h1>
+            <p>Please Train a Model</p>
+            <form method="get" action="/">
+                <button type="submit">Restart</button>
+            </form>
+            """, 400)
+    score = session['model'].test_model()
     return """
     <h1>Test Results</h1>
     <p>Model: {trained}</p>
     <p>Score: {score}</p>
     <img src='/plot.png' alt='Plot'>
     <form method="get" action="/">
-        <button type="submit">Go Home</button>
+        <button type="submit">Restart</button>
     </form>
     """.format(trained=model.trained, score=score)
 
@@ -83,7 +87,7 @@ def add_header(r):
 
 @app.route('/plot.png')
 def make_plot():
-    return Response(model.plot_roc(), mimetype='image/png')
+    return Response(session['model'].plot_roc(), mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
